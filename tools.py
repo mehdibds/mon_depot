961a2000fb72b5e25787f1696e5b51a1e6fd488e
BOUDISSA <3670404@ppti-14-303-10.ufr-info-p6.jussieu.fr>
2019-02-11 17:05:09 +0100

    update

--- tools.py
+++ tools.py
@@ -1,56 +1,57 @@
 # coding: utf-8
 from soccersimulator import Vector2D, MobileMixin , SoccerAction
 from soccersimulator.settings import *
-"""class RandomStrategy(Strategy):
-    def __init__(self):
-        Strategy.__init__(self, "Random")
-
-    def compute_strategy(self, state, id_team, id_player):
-        # id_team is 1 or 2
-        # id_player starts at 0
-        return SoccerAction(Vector2D.create_random(-1,1),
-                            Vector2D.create_random(-1,1))"""
 
 class SuperState():
     def __init__(self,state,id_team,id_player):
         self.state = state
         self.id_team = id_team
         self.id_player = id_player
-        
+   
+	#positon de la balle     
     @property
     def ball(self):
         return self.state.ball.position
     
+     #vitesse de la balle  
+    @property
+    def vitesse_ball(self):
+        return self.state.ball.position
+    
+	#position du joueur
     @property
     def player(self):
         return self.state.player_state(self.id_team,self.id_player).position
     
+	#position du goal
     @property
     def goal(self):
         if (self.id_team==1):
             return Vector2D(GAME_WIDTH,GAME_HEIGHT /2)
         else :
             return Vector2D(0, GAME_HEIGHT/2)
-    
+	#aller vers la balle
     @property
     def go(self):
             return SoccerAction(self.ball -self.player)
     
+	#shooter dans la balle ou aller vers la balle
     @property    
     def shoot(self):
         if (self.player.distance(self.ball) < PLAYER_RADIUS + BALL_RADIUS):
             return SoccerAction(shoot=((self.goal-self.player)/20)*maxPlayerShoot)
         else:
             return SoccerAction((self.ball-self.player)*maxPlayerAcceleration)
-    
-    def etat(self, x, y):
+    	
+	#aller à un point donné
+    def go_to_position(self, x, y):
         pos = Vector2D(x,y)
         return SoccerAction(pos-self.player)
-        
+
+	#trouver l'adversaire le plus proche        
     @property
-    def advproche(self):
+    def adv_proche(self):
         mini=GAME_WIDTH
-        
         for el in self.state.players :
             equipe = el[0]
             joueur = el[1]
@@ -59,25 +60,32 @@ class SuperState():
                     mini = self.player.distance(self.state.player_state(equipe,joueur).position)
                     adverse = joueur
         return adverse
-    
+   
+	#trouver l'équipier le plus proche
     @property
-    def eqproche(self):
+    def eq_proche(self):
         mini=GAME_WIDTH
-        
         for el in self.state.players :
             equipe = el[0]
             joueur = el[1]
             if equipe == self.id_team :
                 if (self.player.distance(self.state.player_state(equipe,joueur).position)<mini):
                     mini = self.player.distance(self.state.player_state(equipe,joueur).position)
-                    equipier = joueur
+                    equipier = self.state.player_state(equipe,joueur).position
         return equipier
-    
+
+	#faire la passe à l'équipier le plus proche
     @property
+    def passe(self):
+        return SoccerAction(shoot=((self.eq_proche-self.player)/20)*maxPlayerShoot)
+
+    """@property
     def tirer(self):
         return SoccerAction(shoot=(self.goal - self.ball).normalize()*maxPlayerShoot)
     
+    @property
     def foncer_but(self):
-        return SoccerAction(shoot=(self.goal - self.ball).normalize())
+        return SoccerAction(shoot=(self.goal - self.ball).normalize())"""
     
     
+	
--- travail.py
+++ travail.py
@@ -15,35 +15,45 @@ from tools import *
 
 class Attaquant(Strategy):
     def __init__(self):
-        Strategy.__init__(self, "RandomAtt")
+        Strategy.__init__(self, "Attaquant")
 
     def compute_strategy(self, state, id_team, id_player):
         # id_team is 1 or 2
         # id_player starts at 0
         s = SuperState(state, id_team, id_player)
-        return s.shoot 
+        return s.shoot
+        
+        """if s.id_team == 1 and id_team==0:
+            if (s.ball.x <= (GAME_WIDTH/2)) :
+                return s.shoot
+            else :
+                return s.passe
+        if s.id_team == 2 :
+            #si la balle se trouve dans le côté droit
+            if (s.ball.x >= GAME_WIDTH/2) and id_team==1 :
+               return s.shoot
+            else :
+                return s.passe"""
          
 class Defenseur(Strategy):
     def __init__(self):
-        Strategy.__init__(self, "RandomDef")
+        Strategy.__init__(self, "Defenseur")
 
     def compute_strategy(self, state, id_team, id_player):
         s = SuperState(state,id_team,id_player)
-        #joueur 3 de l'équipe 1
         if s.id_team == 1 and s.id_player == 1:
-            #si la balle se trouve dans le côté supérieur gauche
+            #si la balle se trouve dans le côté gauche
             if (s.ball.x < GAME_WIDTH/2) :
                 return s.shoot
             else :
-                return s.etat(GAME_WIDTH/4, GAME_HEIGHT/2)
-        #joueur 3 de l'équipe 2
+                return s.go_to_position(GAME_WIDTH/4, GAME_HEIGHT/2)
         if s.id_team == 2 and s.id_player == 1 :
-            #si la balle se trouve dans le côté supérieur droit
+            #si la balle se trouve dans le côté droit
             if (s.ball.x > GAME_WIDTH/2) :
                return s.shoot
             else :
-                return s.etat(3*GAME_WIDTH/4, GAME_HEIGHT/2)
-        #joueur 4 de l'équipe 2
+                return s.go_to_position(3*GAME_WIDTH/4, GAME_HEIGHT/2)
+        
 
 # Create teams
 team1 = SoccerTeam(name="Team 1")
@@ -52,7 +62,7 @@ team2 = SoccerTeam(name="Team 2")
 # Add players
 team1.add("Mbappe", Attaquant())
 team1.add("Kimpembe", Defenseur())
-
+team1.add("Angel Di Maria",Attaquant())
 
   # Random strategy
 team2.add("Dembele", Attaquant())
