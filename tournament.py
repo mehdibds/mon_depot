d195457d027fbf4ff59ac0e5e74bdccdd2173a21
lynasaoucha <lynasaoucha@gmail.com>
2019-03-16 11:51:13 +0100

    test2

---
+++ tournament.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from octogone import *
+from soccersimulator import show_simu, Simulation
+
+  
+#Check team with 1 player and 2 players
+team1=get_team(1)
+team2=get_team(2)
+    
+#Create a match
+#simulate and display the match
+simu = Simulation(team1, team2)
+show_simu(simu)    
